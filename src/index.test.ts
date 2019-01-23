844dd2396e9f87115da319b36ad22f0486d168e1
Jaga Apple <jagaapple@uniboar.com>
2019-01-24 05:32:48 +0900

    [Add] Add tests for entry point file
    - Add tests for entry point file but does not write all patterns

---
+++ src/index.test.ts
@@ -0,0 +1,30 @@
+// =============================================================================================================================
+// SRC - INDEX TEST
+// =============================================================================================================================
+/* tslint:disable:only-arrow-functions no-unused-expression no-null-keyword */
+import { expect } from "chai";
+import * as sinon from "sinon";
+import marubatsu from "./index";
+
+describe("[ Marubatsu ]", function() {
+  afterEach(function() {
+    sinon.restore();
+  });
+
+  // TODO
+  it("should work", function() {
+    const isValid = marubatsu()
+      .string({ length: 3 })
+      .test("abc");
+
+    expect(isValid).to.be.true;
+  });
+
+  context("when calling unknown property,", function() {
+    it("should return undefined", function() {
+      const validator = marubatsu() as any;
+
+      expect(validator.foo).to.be.undefined;
+    });
+  });
+});
