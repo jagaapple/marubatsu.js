933fcd67b2fd071e72a07adb8d6e31d94eb2bf1b
Jaga Apple <jagaapple@uniboar.com>
2019-02-13 05:37:54 +0900

    [Clean] Follow linters
    - Follow linters

--- src/operators/shared/operator.ts
+++ src/operators/shared/operator.ts
@@ -3,12 +3,12 @@
 // =============================================================================================================================
 import { CheckResult } from "@checkers/index";
 
-export type ValidationExecutor = ((value: any) => CheckResult<any>);
+export type ValidationExecutor = (value: any) => CheckResult<any>;
 export interface Validators {
   [name: string]: ValidationExecutor;
 }
 
 export interface Operator {
   name: string;
-  createValidators: ((...args: any) => Validators);
+  createValidators: (...args: any) => Validators;
 }
